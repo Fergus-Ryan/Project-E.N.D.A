6ef76d43e1f5f6cd64d5f2c54fc14473ead6cb87
Fergus-Ryan <fergusryan@live.ie>
2016-11-14 18:08:57 +0000

    The latest version of the code, i have got the apis for facebook, twitter and google plus connected up

---
+++ server/social-config.js
@@ -0,0 +1,19 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook',
+    service: 'twitter',
+    service: 'google'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '328368490880971',
+    secret: '4e8295dc75f447c9e034a822ccb3fe92',
+
+    service: 'twitter',
+    appId: 'QUZZjvHgHxvwWcovS43Eu4RUT',
+    secret: 'ehDrKKMkJLhxhvSfpVGy0HT4yu5mv7lTMplMfnmYS0KpKCR9Lz',
+
+    service: 'google',
+    appId: '7608254878-ji89n2rsim0k1mu1j86ok04lsgkippmp.apps.googleusercontent.com',
+    secret: '5Z4pJD4QOKMxkwy-kZdBI3jL'
+});
